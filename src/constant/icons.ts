e5f2da98278dbd93c45000414fbef6854a6e7e2d
Teerapong <teera04164@gmail.com>
2024-12-16 18:35:31 +0700

    no message

--- src/constant/icons.ts
+++ src/constant/icons.ts
@@ -1,7 +1,7 @@
 export const ICONS_PATH = {
     FAVORITE_GOLD: '/icons/favorite-gold.svg',
     FAVORITE_GRAY: '/icons/favorite-gray.svg',
-    LOGO: '/logo.png',
+    LOGO: '/logo/logo.png',
     LOADING: '/icons/loading.svg',
     CLOSE: '/icons/close.svg',
     SEARCH: '/icons/search.svg',
