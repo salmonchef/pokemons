5f44c938b94a47fa91d289fca9c68ce674863ca4
Teerapong <teera04164@gmail.com>
2024-12-16 18:45:00 +0700

    no message

--- src/services/instance.ts
+++ src/services/instance.ts
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 export const PokemonInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    // baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    baseURL: "https://pokeapi.co/api/v2",
     timeout: 5000,
     headers: {
       "Content-Type": "application/json",
